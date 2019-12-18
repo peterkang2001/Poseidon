85fa848117e833a5f095f91f9a9aab21bd86086e
peterkang2001 <peterkang2001@gmail.com>
2019-12-18 16:00:49 +0800

    新增文件目录存在判断

---
+++ poseidon/core/exceptions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+   Author:       kangliang
+   date:         2019-12-17
+"""
+
+
+class FileNotFoundException(Exception):
+    """
+    文件不存在异常
+    """
+    pass
+
+
+class DirectoryNotFoundException(Exception):
+    """
+    目录不存在异常
+    """
+    pass
