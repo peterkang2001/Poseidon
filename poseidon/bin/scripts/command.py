b48e5acd1ebcc314d1118581c732cc7f98f36d91
peterkang2001 <peterkang2001@gmail.com>
2019-12-19 14:36:16 +0800

    修改实现

--- poseidon/bin/scripts/command.py
+++ poseidon/bin/scripts/command.py
@@ -4,15 +4,21 @@
    Author:       kangliang
    date:         2019-12-16
 """
-import os
 from poseidon.core.files import utils
+from poseidon.core import output
 
 
-def command_start(name):
-    # 暂时将工作目录设置为project_path
-    _home = utils.get_project_path_info().get("project_path")
-    utils.chdir_to_project()
-    # if utils.dir_is_exists(name):
-    #     print("yes")
-    # else:
-    #     print("no")
+def command_start(project_name):
+    _work_path = utils.get_workpath()
+    _project_path = utils.get_project_path(project_name)
+    # utils.chdir_to_project_home_path()
+
+    if not utils.dir_is_exists(project_name):
+        if utils.mkdirs(project_name):
+            output.info("创建目录 {} 成功".format(project_name))
+        # utils.copy_tpl_tree(project_name, 'business')
+    #     # utils.copy_tpl_tree('data')
+    #     # utils.copy_tpl_tree('testcase')
+    else:
+        output.err("创建目录 {} 失败: 该目录已存在".format(project_name))
+        return 1
