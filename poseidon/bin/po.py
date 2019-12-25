760e56bfb6e3a6f349d985e292d53148392358d1
songmengyun <songmengyun@hujiang.com>
2019-12-25 15:10:05 +0800

    优化po-admin

--- poseidon/bin/po.py
+++ poseidon/bin/po.py
@@ -8,8 +8,12 @@ import click
 from poseidon.bin.scripts.command import command_start
 
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version='1.0.0')
 def cli():
+    """poseido-admin：简单创建自动化脚本"""
     pass
 
 @cli.command()
